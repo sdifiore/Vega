5c7e0f666320c3c3d2dec95951fd2b71bbefc9cc
Sergio Di Fiore <difiores@outlook.com>
2018-08-08 09:46:49 -0300

    Add project files.

---
+++ vega/ClientApp/src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { VehicleService } from './services/vehicle.service';
+import { AppErrorHandler } from './services/app.error-handler';
+import { ToastyModule } from 'ng2-toasty';
+import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list';
+import { ViewVehicleComponent } from './view-vehicle/view-vehicle';
+import { PaginationComponent } from './shared/pagination.component';
+import { Http, HttpModule } from '@angular/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    CounterComponent,
+    HomeComponent,
+    VehicleFormComponent,
+    VehicleListComponent,
+    ViewVehicleComponent,
+    CounterComponent,
+    FetchDataComponent,
+    PaginationComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    HttpModule,
+    FormsModule,
+    ToastyModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
+            { path: 'vehicles/new', component: VehicleFormComponent },
+            { path: 'vehicles/edit/:id', component: VehicleFormComponent },
+            { path: 'vehicles/:id', component: ViewVehicleComponent },
+            { path: 'vehicles', component: VehicleListComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'home' }
+    ])
+  ],
+  providers: [
+    { provide: ErrorHandler, useClass: AppErrorHandler },,
+    VehicleService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
